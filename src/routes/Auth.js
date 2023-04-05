5ad50b33c2ec46fa3d599c272e84af169f87d030
yanJuicy <hanjub3@gmail.com>
2023-04-05 19:45:15 +0900

    firebase 로그인 설정

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,1 +1,34 @@
-export const Auth = () => <span>Auth</span>;
+import {useState} from "react";
+
+export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" value={email} onChange={onChange} type="email" placeholder="Email" required/>
+                <input name="password" value={password} onChange={onChange} type="password" placeholder="Password"
+                       required/>
+                <input type="submit" value="Log In"/>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
